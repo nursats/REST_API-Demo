ad8373f3cabace0231adf8665893229d03db4a96
Nursat Seitov <nursat.seitov12@gmail.com>
2024-12-05 20:22:27 +0500

    weather api error handling

--- app/services/weather_api.py
+++ app/services/weather_api.py
@@ -2,19 +2,31 @@ from app.config import API_KEY, BASE_URL
 import requests
 
 def fetch_weather(city: str):
-
-    #Fetch the weather for a specific city.
-
+    """
+    Fetch the weather for a specific city.
+    """
     params = {"q": city, "appid": API_KEY, "units": "metric"}
 
-    response = requests.get(BASE_URL, params=params)
-    response.raise_for_status()
+    try:
+        response = requests.get(BASE_URL, params=params)
+        response.raise_for_status()
+        
+        res = response.json()
+        
+        if "city" in res and "list" in res and len(res["list"]) > 0:
+            city_name = res["city"]["name"]
+            temperature = res["list"][0]["main"]["temp"]
+            return {"city": city_name, "temperature": temperature}
+        else:
+            return {"error": "Unexpected response format"}
+    except requests.exceptions.HTTPError as http_err:
+        if response.status_code == 404:
+            return {"error": "City not found"}
+        else:
+            return {"error": f"HTTP error occurred: {http_err}"}
+    except Exception as err:
+        return {"error": f"An error occurred: {err}"}
 
-    res = response.json()
-    city_name = res["city"]["name"]
-    temperature = res["list"][0]["main"]["temp"]
-
-    return {"city": city_name, "temperature": temperature}
 
 
 
